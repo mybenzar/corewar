9d5b0204797869aef7f01ff7bb157bb2fc31e54d
Myriam BENZARTI <mybenzar@student.42.fr>
2019-08-27 18:08:16 +0200

    [DEV] op_live writing in progress

---
+++ srcs/op_live.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   op_live.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mybenzar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/27 17:44:28 by mybenzar          #+#    #+#             */
+/*   Updated: 2019/08/27 18:07:48 by mybenzar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "corewar.h"
+
+/*
+**	--> op_live : launches live operation (0x01) that holds on 4 bytes
+**	that represent the player's id
+*/
+
+int		op_live(char *param_str)
+{
+	int	id;
+
+	id = ft_atoi_base
+	ft_printf("Player %d is alive!\n", id);
+}
