30b21c735dd39990dd7bff7bf99d5e6b545e98de
Myriam BENZARTI <mybenzar@student.42.fr>
2019-08-28 17:36:48 +0200

    [FEAT] added first version of op_live and added a the t_arg structure in corewar.h

--- srcs/op_live.c
+++ srcs/op_live.c
@@ -6,21 +6,22 @@
 /*   By: mybenzar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/27 17:44:28 by mybenzar          #+#    #+#             */
-/*   Updated: 2019/08/27 18:07:48 by mybenzar         ###   ########.fr       */
+/*   Updated: 2019/08/28 17:36:08 by mybenzar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
+#include "ft_printf.h"
 
 /*
 **	--> op_live : launches live operation (0x01) that holds on 4 bytes
 **	that represent the player's id
 */
 
-int		op_live(char *param_str)
+int		op_live(t_corewar *cor, t_arg *arg)
 {
 	int	id;
 
-	id = ft_atoi_base
+	id = ft_atoi_base(arg.arg1);
 	ft_printf("Player %d is alive!\n", id);
 }
